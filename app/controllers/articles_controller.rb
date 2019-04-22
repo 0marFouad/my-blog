561cf5ce96a600339be502f08ae217fa482b7b6c
OmarFouad <omar.ahmed.fouad1996@gmail.com>
2019-04-22 07:43:34 +0200

    Added Index Page and Some Links for Navigation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController < ApplicationController
+
+
+  def index
+    @articles = Article.all
+  end
+
+
   def new
     @article = Article.new
   end
@@ -11,6 +18,20 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully Updated!"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def show
