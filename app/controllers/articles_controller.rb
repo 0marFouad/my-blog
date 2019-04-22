3853aad3339faa80fe6b702094820e3a37561d93
OmarFouad <omar.ahmed.fouad1996@gmail.com>
2019-04-22 08:12:14 +0200

    Adding Partials and Destroy Functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,13 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully Deleted!"
+    redirect_to articles_path
+  end
+
 
   def new
     @article = Article.new
